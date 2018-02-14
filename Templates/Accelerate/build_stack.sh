7e707cf7ebf1dcf26702362900c10560e8fa4049
Mike Wooten <mwooten@fortinet.com>
2018-02-14 08:38:48 -0600

    Convert Everything to YAML. Add 5.6.3 ami’s, incorporate haberra’s changes

--- Templates/Accelerate/build_stack.sh
+++ Templates/Accelerate/build_stack.sh
@@ -3,12 +3,13 @@ stack1=acceleratebase
 stack2=addprivatelinux
 stack3=addpubliclinux
 stack4=acceleratefgt
-region=us-west-2
-instance_type=m3.medium
-key=mdwadmin-oregon
-domain="fortimdw.com"
-fgtdns="fortias"
-httpsdns="httpserver"
+region=us-west-1
+instance_type=c4.large
+key=ftntkey_californmia
+health_check_port=22
+domain=fortidevelopment.com
+fgtdns=fortias
+webdns=httpserver
 access="0.0.0.0/0"
 
 #
@@ -17,7 +18,7 @@ access="0.0.0.0/0"
 count=`aws cloudformation list-stacks --stack-status-filter CREATE_COMPLETE --region "$region" |grep "$stack1" |wc -l`
 if [ "${count}" -eq "0" ]
 then
-    aws cloudformation create-stack --stack-name "$stack" --region "$region" --template-body file://NewVPC_BaseSetup.template \
+    aws cloudformation create-stack --stack-name "$stack1" --region "$region" --template-body file://NewVPC_BaseSetup.yaml \
         --parameters ParameterKey=VPCCIDR,ParameterValue=10.0.0.0/16 \
          ParameterKey=Public2Subnet,ParameterValue=10.0.2.0/24 \
          ParameterKey=Public1Subnet,ParameterValue=10.0.0.0/24 \
@@ -45,7 +46,7 @@ done
 # Pull the outputs from the first template
 #
 tfile=$(mktemp /tmp/foo.XXXXXXXXX)
-aws cloudformation --region us-west-2 describe-stacks --stack-name "$stack1" --output text --query 'Stacks[*].Outputs[*].{KEY:OutputKey,Value:OutputValue}' > $tfile
+aws cloudformation --region "$region" describe-stacks --stack-name "$stack1" --output text --query 'Stacks[*].Outputs[*].{KEY:OutputKey,Value:OutputValue}' > $tfile
 VPC=`cat $tfile|grep ^VPCID|cut -f2 -d$'\t'`
 AZ1=`cat $tfile|grep ^AZ1|cut -f2 -d$'\t'`
 AZ2=`cat $tfile|grep ^AZ2|cut -f2 -d$'\t'`
@@ -60,102 +61,79 @@ then
 fi
 
 #
-# Now deploy linux instances in the private subnets on top of the existing VPC
+# Now deploy linux web server instances in the private subnets on top of the existing VPC
 #
 count=`aws cloudformation list-stacks --stack-status-filter CREATE_COMPLETE --region "$region" |grep "$stack2" |wc -l`
 if [ "${count}" -eq "0" ]
 then
     aws cloudformation create-stack --stack-name "$stack2" --region "$region" --capabilities CAPABILITY_IAM \
-        --template-body file://ExistingVPC_WebLinuxInstances.template \
+        --template-body file://ExistingVPC_WebLinuxInstances.yaml \
         --parameters    ParameterKey=VPCID,ParameterValue="$VPC" \
                     ParameterKey=AZForInstance1,ParameterValue="$AZ1" \
                     ParameterKey=AZForInstance2,ParameterValue="$AZ2" \
-                    ParameterKey=Public1Subnet,ParameterValue="$SUBNET3" \
-                    ParameterKey=Public2Subnet,ParameterValue="$SUBNET4" \
+                    ParameterKey=Private1Subnet,ParameterValue="$SUBNET2" \
+                    ParameterKey=Private2Subnet,ParameterValue="$SUBNET4" \
                     ParameterKey=CIDRForInstanceAccess,ParameterValue="$access" \
                     ParameterKey=InstanceType,ParameterValue="$instance_type" \
-                    ParameterKey=KeyPair,ParameterValue="$key"
+                    ParameterKey=KeyPair,ParameterValue="$key" \
+                    ParameterKey=HealthCheckPort,ParameterValue="$health_check_port" \
+                    ParameterKey=DomainName,ParameterValue="$domain" \
+                    ParameterKey=WebDNSPrefix,ParameterValue="$webdns"
 fi
+
 #
-# Now deploy linux instances in the public subnets on top of the existing VPC
+# Now deploy linux traffic generator instances in the public subnets on top of the existing VPC
 #
 count=`aws cloudformation list-stacks --stack-status-filter CREATE_COMPLETE --region "$region" |grep "$stack3" |wc -l`
 if [ "${count}" -eq "0" ]
 then
     aws cloudformation create-stack --stack-name "$stack3" --region "$region" --capabilities CAPABILITY_IAM \
-        --template-body file://ExistingVPC_AddLinuxInstances.template \
+        --template-body file://ExistingVPC_AddLinuxInstances.yaml \
         --parameters    ParameterKey=VPCID,ParameterValue="$VPC" \
                     ParameterKey=AZForInstance1,ParameterValue="$AZ1" \
                     ParameterKey=AZForInstance2,ParameterValue="$AZ2" \
                     ParameterKey=Public1Subnet,ParameterValue="$SUBNET1" \
-                    ParameterKey=Public2Subnet,ParameterValue="$SUBNET2" \
+                    ParameterKey=Public2Subnet,ParameterValue="$SUBNET3" \
                     ParameterKey=CIDRForInstanceAccess,ParameterValue="$access" \
                     ParameterKey=InstanceType,ParameterValue="$instance_type" \
                     ParameterKey=KeyPair,ParameterValue="$key"
 fi
 
+#
+# Wait for template above to CREATE_COMPLETE
+#
+for (( c=1; c<=10; c++ ))
+do
+    count=`aws cloudformation list-stacks --stack-status-filter CREATE_COMPLETE --region "$region" |grep "$stack3" |wc -l`
+    if [ ${count} -eq 1 ]
+    then
+        break
+    fi
+    echo $c
+    sleep 30
+done
+
+#
+# Now deploy fortigate autoscaling instances in the public & private subnets on top of the existing VPC
+#
 count=`aws cloudformation list-stacks --stack-status-filter CREATE_COMPLETE --region "$region" |grep "$stack4" |wc -l`
 if [ "${count}" -eq "0" ]
 then
     aws cloudformation create-stack --stack-name "$stack4" --region "$region" --capabilities CAPABILITY_IAM \
-        --template-body file://ExistingVPC_Fortigate542_Autoscale_ELB_WorkerNode.template \
-        --parameters    ParameterKey=ASGroupMaxSize,ParameterValue=5 \
-                        ParameterKey=ASKeyPair,ParameterValue="$key" \
-                        ParameterKey=ASQueue,ParameterValue="$stack4"q \
-                        ParameterKey=AZForFirewall1,ParameterValue="$AZ1" \
-                        ParameterKey=AZForFirewall2,ParameterValue="$AZ2" \
-                        ParameterKey=CIDRForASAccess,ParameterValue="$access" \
-                        ParameterKey=CIDRForFortiGateAccess,ParameterValue="$access" \
-                        ParameterKey=FortiGateInstanceType,ParameterValue="$instance_type" \
-                        ParameterKey=FortiGateKeyPair,ParameterValue="$key" \
+        --template-body file://ExistingVPC_AddFortigates.yaml \
+        --parameters    ParameterKey=CIDRForFortiGateAccess,ParameterValue="$access" \
+                        ParameterKey=FortiGateEC2Type,ParameterValue="$instance_type" \
+                        ParameterKey=HealthCheckPort,ParameterValue=541 \
                         ParameterKey=DomainName,ParameterValue="$domain" \
                         ParameterKey=FGTDNSPrefix,ParameterValue="$fgtdns" \
-                        ParameterKey=HTTPSDNSPrefix,ParameterValue="$httpsdns" \
-                        ParameterKey=HealthCheckPort,ParameterValue=541 \
-                        ParameterKey=HealthCheckPort,ParameterValue=541 \
-                        ParameterKey=InitialFortiGateInstanceType,ParameterValue="$instance_type" \
-                        ParameterKey=InternalHealthCheckPort,ParameterValue=80 \
-                        ParameterKey=InternalListenerPort,ParameterValue=80 \
-                        ParameterKey=ListenerPort,ParameterValue=80 \
-                        ParameterKey=Private1Subnet,ParameterValue="$SUBNET2" \
-                        ParameterKey=Private2Subnet,ParameterValue="$SUBNET4" \
-                        ParameterKey=Public1Subnet,ParameterValue="$SUBNET1" \
-                        ParameterKey=Public2Subnet,ParameterValue="$SUBNET3" \
-                        ParameterKey=ScaleDownThreshold,ParameterValue=40 \
-                        ParameterKey=ScaleUpThreshold,ParameterValue=80 \
-                        ParameterKey=ScalingParameter,ParameterValue=CPUUtilization \
-                        ParameterKey=ScalingPeriod,ParameterValue=300 \
+                        ParameterKey=SubnetID2,ParameterValue="$SUBNET2" \
+                        ParameterKey=SubnetID4,ParameterValue="$SUBNET4" \
+                        ParameterKey=SubnetID1,ParameterValue="$SUBNET1" \
+                        ParameterKey=SubnetID3,ParameterValue="$SUBNET3" \
+                        ParameterKey=AZForFirewall1,ParameterValue="$AZ1" \
+                        ParameterKey=AZForFirewall2,ParameterValue="$AZ2" \
                         ParameterKey=VPCID,ParameterValue="$VPC"
 fi
-
-
-
-    aws cloudformation create-stack --stack-name "$stack4" --region "$region" --capabilities CAPABILITY_IAM \
-        --template-body file://ExistingVPC_Fortigate542_Autoscale_ELB_WorkerNode.template \
-        --parameters    ParameterKey=ASGroupMaxSize,ParameterValue=5 \
-                        ParameterKey=ASKeyPair,ParameterValue="$key" \
-                        ParameterKey=ASQueue,ParameterValue="$stack4"q \
-                        ParameterKey=AZForFirewall1,ParameterValue="$AZ1" \
-                        ParameterKey=AZForFirewall2,ParameterValue="$AZ2" \
-                        ParameterKey=CIDRForASAccess,ParameterValue="$access" \
-                        ParameterKey=CIDRForFortiGateAccess,ParameterValue="$access" \
-                        ParameterKey=FortiGateInstanceType,ParameterValue="$instance_type" \
-                        ParameterKey=FortiGateKeyPair,ParameterValue="$key" \
-                        ParameterKey=DomainName,ParameterValue="$domain" \
-                        ParameterKey=FGTDNSPrefix,ParameterValue="$fgtdns" \
-                        ParameterKey=HTTPSDNSPrefix,ParameterValue="$httpsdns" \
-                        ParameterKey=HealthCheckPort,ParameterValue=541 \
-                        ParameterKey=HealthCheckPort,ParameterValue=541 \
-                        ParameterKey=InitialFortiGateInstanceType,ParameterValue="$instance_type" \
-                        ParameterKey=InternalHealthCheckPort,ParameterValue=80 \
-                        ParameterKey=InternalListenerPort,ParameterValue=80 \
-                        ParameterKey=ListenerPort,ParameterValue=80 \
-                        ParameterKey=Private1Subnet,ParameterValue="$SUBNET2" \
-                        ParameterKey=Private2Subnet,ParameterValue="$SUBNET4" \
-                        ParameterKey=Public1Subnet,ParameterValue="$SUBNET1" \
-                        ParameterKey=Public2Subnet,ParameterValue="$SUBNET3" \
-                        ParameterKey=ScaleDownThreshold,ParameterValue=40 \
-                        ParameterKey=ScaleUpThreshold,ParameterValue=80 \
-                        ParameterKey=ScalingParameter,ParameterValue=CPUUtilization \
-                        ParameterKey=ScalingPeriod,ParameterValue=300 \
-                        ParameterKey=VPCID,ParameterValue="$VPC"
+#
+# End of the script
+#
